56e8895b4d0a852c23d4b20379989599566259cc
YU(James) DU <csjamesdu@gmail.com>
2020-02-29 00:56:38 +1100

    put query name list into config files

--- src/main/java/com/csjamesdu/datafeed/service/TaskRunner.java
+++ src/main/java/com/csjamesdu/datafeed/service/TaskRunner.java
@@ -3,12 +3,11 @@ package com.csjamesdu.datafeed.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,29 +22,27 @@ public class TaskRunner {
     @Autowired
     private DataReadService dataReadService;
 
+    @Value("#{'${my.list.of.tables}'.split(',')}")
+    private List<String> tables;
+
     @Scheduled(fixedRate = 10000, initialDelay = 1000)
     public void runTask() {
 
-        List<String> tableNames = new ArrayList<>();
-        tableNames.add("actor");
-        tableNames.add("address");
-        tableNames.add("city");
-        tableNames.add("category");
-        tableNames.add("country");
-
-        tableNames.forEach(name->taskProcess(name));
+        tables.forEach(name->taskProcess(name));
 
     }
 
     private void taskProcess(String queryName){
-        LOGGER.info("Start exporting file "+ queryName + " {} ", dateFormat.format(new Date()));
-        dataReadService.export(queryName);
         try {
+            LOGGER.info("*******************************************");
+            LOGGER.info("Start exporting file "+ queryName + " {} ", dateFormat.format(new Date()));
+            dataReadService.export(queryName);
+            LOGGER.info("Finish exporting file " + queryName + " {} ", dateFormat.format(new Date()));
+            LOGGER.info("*******************************************");
             sleep(1000);
         } catch (InterruptedException e) {
             LOGGER.warn("Thread sleep is interrupted: " + e);
         }
-        LOGGER.info("Finish exporting file " + queryName + " {} ", dateFormat.format(new Date()));
 
     }
 }
